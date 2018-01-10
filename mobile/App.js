825b9e3dac6d0504cc891ab2cc3e9644c5fbe6a6
Michael Arick <michaelarick@gmail.com>
2018-01-09 19:39:31 -0800

    Add a buttongroup

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,45 @@
 import React from 'react'
 import { StyleSheet, Text, View, Image } from 'react-native'
-import { Button } from 'react-native-elements'
+import { ButtonGroup, Button } from 'react-native-elements'
 
 export default class App extends React.Component {
-  render() {
+  constructor () {
+    super()
+    this.state = {
+      selectedIndex: 2
+    }
+    this.updateIndex = this.updateIndex.bind(this)
+  }
+
+  updateIndex (selectedIndex) {
+    this.setState({selectedIndex})
+  }
+
+  render () {
+    const buttons = ['Hello', 'World', 'Buttons']
+    const { selectedIndex } = this.state
+
     return (
       <View style={styles.container}>
         <Text>Life Tracking App</Text>
-        <Image
-          style={{width: 150, height: 150}}
-          source={{uri: 'https://media.giphy.com/media/CO8wN7YrZ3X0Y/giphy.gif'}}
-        />
-        <Button
-          raised
-          icon={{name: 'home', size: 32}}
-          buttonStyle={{backgroundColor: 'red', borderRadius: 10}}
-          textStyle={{textAlign: 'center'}}
-          title={`Welcome to\nReact Native Elements`}
-        />
+        <ButtonGroup
+          onPress={this.updateIndex}
+          selectedIndex={selectedIndex}
+          buttons={buttons}
+          containerStyle={{height: 100}} />
+         <Image
+           style={{width: 150, height: 150}}
+           source={{uri: 'https://media.giphy.com/media/CO8wN7YrZ3X0Y/giphy.gif'}}
+         />
+         <Button
+           raised
+           icon={{name: 'home', size: 32}}
+           buttonStyle={{backgroundColor: 'red', borderRadius: 10}}
+           textStyle={{textAlign: 'center'}}
+           title={`Welcome to\nReact Native Elements`}
+         />
       </View>
-    );
+    )
   }
 }
 
